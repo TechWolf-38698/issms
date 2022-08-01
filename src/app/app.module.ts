711a36450ad2e5eb14bc558cb786635ca83a58f0
Tech Wolf <techwolf38698@gmail.com>
2022-08-01 17:14:25 +0500

    1st Aug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +39,7 @@ import { IndexAdminStaffComponent } from './pages/AdminStaff/index-admin-staff/i
     ManageAdminStaffComponent,
     IndexAdminStaffComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/Students/add-students/add-students.component.ts
+++ src/app/pages/Students/add-students/add-students.component.ts
@@ -1,12 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-students',
-  templateUrl: './add-students.component.html',
   styleUrls: ['./add-students.component.css'],
+  templateUrl: './add-students.component.html',
 })
 export class AddStudentsComponent implements OnInit {
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
+
+  loading = false;
+
+  form = this.fb.group({
+    admissionNo: ['string'],
+    firstName: ['string'],
+    lastName: ['string'],
+    email: ['string'],
+    dob: ['string'],
+    studentHouse: this.fb.group({
+      cardinal: [false],
+      paramount: [false],
+      eminent: [false],
+    }),
+    studentContactNo: ['string'],
+    gender: ['string'],
+    bloodGroup: ['string'],
+    address: ['string'],
+    profilePic: ['string'],
+    zipCode: ['string'],
+    fatherName: ['string'],
+    fatherSurname: ['string'],
+    fatherEmail: ['string'],
+    fatherNo: ['string'],
+    fatherCNIC: ['string'],
+    fatherProfession: ['string'],
+    fatherCompany: ['string'],
+    motherName: ['string'],
+    motherSurname: ['string'],
+    parentsEmail: ['string'],
+    emergencyContact: ['string'],
+    emergencyContactRelation: ['string'],
+    sibling1Class: ['string'],
+    sibling1Name: ['string'],
+    sibling2Class: ['string'],
+    sibling2Name: ['string'],
+    sibling3Class: ['string'],
+    sibling3Name: ['string'],
+    fbLink: ['string'],
+    linkedinProfile: ['string'],
+  });
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    this.loading = true;
+  }
 }
